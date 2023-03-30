6c34acfeb9027700265ed8f1f87c4c7a91b37354
Jorge Mario Lasagni <jlasagni@gmail.com>
2023-03-30 08:32:35 -0300

    Avanzamos al PF

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,3 +1,61 @@
+// import { useContext, useState } from "react"
+// export const Checkout = () => {
+//     const [values, setValues] = useState({
+//         nombre:     '',
+//         direccion:  '',
+//         email:      ''
+//     })
+
+//     const handleInputChange = (e) => {
+//         //console.log(e.target.name)
+//                 setValues({
+//                      ...values,
+//                     [e.target.name]: e.target.value
+//                 })
+//             }
+
+//     const handleSubmit = (e) => {
+//                 e.preventDefault()
+//             console.log("Submit",values)               }
+
+//     return (
+//         <div className="container my-5">
+//             <h2>CheckOut</h2>
+//             <hr />
+
+//             <form onSubmit={handleSubmit}>
+//                 <input 
+//                     onChange    ={handleInputChange}
+//                     value       ={values.nombre}
+//                     type        ={'text'}
+//                     placeholder ='tu nombre'
+//                     className   ="form-control my-2"
+//                     name        ="nombre"
+//                 />
+//                 <input 
+//                     onChange    ={handleInputChange}
+//                     value       ={values.direccion}
+//                     type        ={'text'}
+//                     placeholder ='tu dirección'
+//                     className   ="form-control my-2"
+//                     name        ="direccion"
+//                 />
+//                 <input
+//                     onChange    ={handleInputChange}
+//                     value       ={values.email}
+//                     type        ={'email'}
+//                     placeholder ='tu email'
+//                     className   ="form-control my-2"
+//                     name        ="email"
+//                 />
+
+//                 <button className="btn btn-primary" type="submit">Enviar Datos</button>
+//             </form>
+
+//         </div>
+//     )
+// }
+
 import { useContext, useState } from "react"
 import { Navigate } from "react-router-dom"
 import { CartContext } from "../../context/CartContext"
@@ -16,7 +74,7 @@ export const Checkout = () => {
     const handleInputChange = (e) => {
         setValues({
             ...values,
-            [e.target.nombre]: e.target.value
+            [e.target.name]: e.target.value
         })
     }
     const handleSubmit = (e) => {
@@ -27,7 +85,7 @@ export const Checkout = () => {
             return
         }
         if (values.direccion.length < 3) {
-            alert("Direccion inválido")
+            alert("Direccion inválida")
             return
         }
         if (values.email.length < 5) {
@@ -44,9 +102,9 @@ export const Checkout = () => {
 
         console.log("Submit", orden)
     }
-
+    // Para no ingresar al Checkout! 
     if (cart.length === 0) {
-        return <Navigate to="/"/>
+        return <Navigate to="/" />
     }
 
     return (
@@ -54,27 +112,27 @@ export const Checkout = () => {
             <h2>CheckOut</h2>
             <hr />
             <form onSubmit={handleSubmit}>
-                <input 
+                <input required
                     onChange={handleInputChange}
                     value={values.nombre}
-                    type={'text'}
-                    placeholder='Tu nombre'
+                    type="text"
+                    placeholder='Ingrese su Nombre'
                     className="form-control my-2"
                     name="nombre"
                 />
-                <input 
+                <input required
                     onChange={handleInputChange}
                     value={values.direccion}
-                    type={'text'}
+                    type='text'
                     placeholder='Direccion'
                     className="form-control my-2"
                     name="direccion"
                 />
-                <input 
+                <input required
                     onChange={handleInputChange}
                     value={values.email}
-                    type={'email'}
-                    placeholder='Tu Email'
+                    type='email'
+                    placeholder='Tu E-Mail'
                     className="form-control my-2"
                     name="email"
                 />
